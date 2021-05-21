d23e2da90c62f397fc7f620dd04dc5832daa0039
SABiagini <71790100+SABiagini@users.noreply.github.com>
2021-05-21 03:03:42 +0200

    Update PiC.sh

--- PiC/PiC.sh
+++ PiC/PiC.sh
@@ -1,48 +1,83 @@
 #!/bin/bash
 
 scripts="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 pwd=$PWD
+
+exists()
+{ 
+  [ -e "$1" ]
+}
+
+#
+## Level Mother
+###
 
 cd $pwd/M
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list && sed -i 's/^/M\t/g' list &&
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/M/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list && sed -i 's/^/M\t/g' list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode.sh &&
-sh $scripts/SumMatrix.sh indcode ../*.coef &&
+sh $scripts/SumMatrix.sh indcode $pwd/*.coef &&
 sed -i 1d PercMatrix.txt &&
 Rscript $scripts/Outliers.R &&
+
+if exists output_*
+then  
 cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
-rm output_* list indcode Perc* Sum* &&
+rm output_* list indcode Perc* Sum* 
+else 
+rm list indcode Perc* Sum* 
+fi
+
+if [ -s remove ]
+then 
 fgrep -wvf remove Community_M.txt > Community_M_filtered.txt &&
 fgrep -wvf remove ../*.coef > coef.${PWD##*/} &&
-
 echo -e "Leaving level M\n"
-
 cd $pwd/MD
+for i in ./*.txt; do fgrep -wvf $pwd/M/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done 
+else 
+cp Community_M.txt Community_M_filtered.txt &&
+cp ../*.coef  coef.${PWD##*/} &&
+echo -e "Leaving level M\n"
+cd $pwd/MD
+fi
 
-for i in ./*.txt; do fgrep -wvf ../M/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-cp ../M/Community_M_filtered.txt . && mv Community_M_filtered.txt Community_M.txt &&
+#
+## Level Daughter I
+###
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+cp $pwd/M/Community_M_filtered.txt . && mv Community_M_filtered.txt Community_M.txt &&
+
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode.sh &&
-sh $scripts/SumMatrix.sh indcode ../M/coef.* &&
+sh $scripts/SumMatrix.sh indcode $pwd/M/coef.* &&
 sed -i 1d PercMatrix.txt &&
 Rscript $scripts/Outliers.R &&
+if exists output_*
+then  
 cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
-rm output_* list indcode Perc* Sum* &&
-for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-fgrep -wvf remove ../M/coef.* > coef.${PWD##*/} &&
+rm output_* list indcode Perc* Sum* 
+else 
+rm list indcode Perc* Sum* 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+if [ -s remove ]
+then 
+for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
+fgrep -wvf remove $pwd/M/coef.* > coef.${PWD##*/} 
+else 
+cp ../M/coef.*  coef.${PWD##*/} 
+fi
+
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode_II.sh 3 &&
 sh $scripts/SumMatrix.sh indcode coef.* &&
-
 tail -n +3 PercMatrix.txt | cut -f 1 > alpha &&
 cut -f2- PercMatrix.txt | tail -n +3 > main &&
 head -n1 PercMatrix.txt > header1 &&
@@ -59,34 +94,49 @@ tr '\n' '\t' < header3 > header3_tr &&
 echo "" >> header3_tr &&
 cat header1 header3_tr main2 > PercMatrix_alpha.txt &&
 rm header1 header2 main2 header2_tr header3 indcode_alpha header3_tr &&
-
 echo -e "Leaving level MD\n"
 
+#
+## Level Daughter II
+###
+
 cd $pwd/MDD
+cat $pwd/*/remove >  $pwd/MDD/REMOVE
 
-for i in ./*.txt; do fgrep -wvf ../M/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-cp ../MD/Community_M* . &&
+if [ -s REMOVE ]
+then
+for i in ./*.txt; do fgrep -wvf $pwd/MDD/REMOVE $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+cp $pwd/MD/Community_M.txt . &&
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode.sh &&
-sh $scripts/SumMatrix.sh indcode ../MD/coef.* &&
+sh $scripts/SumMatrix.sh indcode $pwd/MD/coef.* &&
 sed -i 1d PercMatrix.txt &&
 Rscript $scripts/Outliers.R &&
-
+if exists output_*
+then  
 cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
-rm output_* list indcode Perc* Sum* &&
-for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-fgrep -wvf remove ../MD/coef.* > coef.${PWD##*/} &&
+rm output_* list indcode Perc* Sum* 
+else 
+rm list indcode Perc* Sum* 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+if [ -s remove ]
+then 
+for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
+fgrep -wvf remove $pwd/MD/coef.* > coef.${PWD##*/} 
+else 
+cp ../MD/coef.*  coef.${PWD##*/} 
+fi
+
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
-sh $scripts/indcode_II.sh 4 &&
+sh $scripts/indcode_II.sh 3 &&
 sh $scripts/SumMatrix.sh indcode coef.* &&
-
 tail -n +3 PercMatrix.txt | cut -f 1 > alpha &&
 cut -f2- PercMatrix.txt | tail -n +3 > main &&
 head -n1 PercMatrix.txt > header1 &&
@@ -106,31 +156,46 @@ rm header1 header2 main2 header2_tr header3 indcode_alpha header3_tr &&
 
 echo -e "Leaving level MDD\n"
 
+#
+## Level Daughter III
+###
+
 cd $pwd/MDDD
+cat $pwd/*/remove > $pwd/MDDD/REMOVE
 
-for i in ./*.txt; do fgrep -wvf ../M/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MDD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-cp ../MDD/Community_M* . &&
+if [ -s REMOVE ]
+then
+for i in ./*.txt; do fgrep -wvf $pwd/MDDD/REMOVE $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+cp $pwd/MDD/Community_M.txt . &&
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode.sh &&
-sh $scripts/SumMatrix.sh indcode ../MDD/coef.* &&
+sh $scripts/SumMatrix.sh indcode $pwd/MDD/coef.* &&
 sed -i 1d PercMatrix.txt &&
 Rscript $scripts/Outliers.R &&
+if exists output_*
+then  
 cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
-rm output_* list indcode Perc* Sum* &&
+rm output_* list indcode Perc* Sum* 
+else 
+rm list indcode Perc* Sum* 
+fi
+
+if [ -s remove ]
+then 
 for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-fgrep -wvf remove ../MDD/coef.* > coef.${PWD##*/} &&
+fgrep -wvf remove $pwd/MDD/coef.* > coef.${PWD##*/} 
+else 
+cp ../MDD/coef.*  coef.${PWD##*/} 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
-
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 Rscript $scripts/summary.R list &&
-sh $scripts/indcode_II.sh 5 &&
+sh $scripts/indcode_II.sh 3 &&
 sh $scripts/SumMatrix.sh indcode coef.* &&
-
 tail -n +3 PercMatrix.txt | cut -f 1 > alpha &&
 cut -f2- PercMatrix.txt | tail -n +3 > main &&
 head -n1 PercMatrix.txt > header1 &&
@@ -147,35 +212,50 @@ tr '\n' '\t' < header3 > header3_tr &&
 echo "" >> header3_tr &&
 cat header1 header3_tr main2 > PercMatrix_alpha.txt &&
 rm header1 header2 main2 header2_tr header3 indcode_alpha header3_tr &&
-
 echo -e "Leaving level MDDD\n"
 
+#
+## Level Daughter IV
+###
+
 cd $pwd/MDDDD
+cat $pwd/*/remove > $pwd/MDDDD/REMOVE
 
-for i in ./*.txt; do fgrep -wvf ../M/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MDD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-for i in ./*.txt; do fgrep -wvf ../MDDD/remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-cp ../MDDD/Community_M* . &&
+if [ -s REMOVE ]
+then
+for i in ./*.txt; do fgrep -wvf $pwd/MDDDD/REMOVE $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done 
+fi
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+cp $pwd/MDDD/Community_M.txt . &&
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDDDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
 sh $scripts/indcode.sh &&
-sh $scripts/SumMatrix.sh indcode ../MDDD/coef.* &&
+sh $scripts/SumMatrix.sh indcode $pwd/MDDD/coef.* &&
 sed -i 1d PercMatrix.txt &&
 Rscript $scripts/Outliers.R &&
-cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
-rm output_* list indcode Perc* Sum* &&
-for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
-fgrep -wvf remove ../MDDD/coef.* > coef.${PWD##*/} &&
 
-while IFS=$'\t' read -r id m; do grep $id ./*txt;done< Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
+if exists output_*
+then  
+cut -f1,2 output_* | grep TRUE | cut -f1 | sed -r '/^\s*$/d' > remove &&
+rm output_* list indcode Perc* Sum* 
+else 
+rm list indcode Perc* Sum* 
+fi
+
+if [ -s remove ]
+then 
+for i in ./*.txt; do fgrep -wvf remove $i > ${i%.*}_filtered.txt && rm $i && mv ${i%.*}_filtered.txt $i; done &&
+fgrep -wvf remove $pwd/MDDD/coef.* > coef.${PWD##*/} 
+else 
+cp ../MDDD/coef.*  coef.${PWD##*/} 
+fi
+
+while IFS=$'\t' read -r id m; do grep $id ./*txt;done< $pwd/MDDDD/Community_M.txt | grep -Pv 'ID\tCommunity' | sed 's/\.\/Community_//g' | sed 's/\.txt//g' | sed 's/\:/\t/g' | sed 's/M[0-9]\+D\t/MD\t/g' | sed 's/M[0-9]\+D[0-9]\+D\t/MDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDD\t/g' | sed 's/M[0-9]\+D[0-9]\+D[0-9]\+D[0-9]\+D\t/MDDDD\t/g' > list &&
 
 Rscript $scripts/summary.R list &&
-sh $scripts/indcode_II.sh 6 &&
+sh $scripts/indcode_II.sh 3 &&
 sh $scripts/SumMatrix.sh indcode coef.* &&
-
 tail -n +3 PercMatrix.txt | cut -f 1 > alpha &&
 cut -f2- PercMatrix.txt | tail -n +3 > main &&
 head -n1 PercMatrix.txt > header1 &&
@@ -198,4 +278,3 @@ echo -e "Leaving level MDDDD\n"
 cd ..
 
 echo -e "Done!\n"
-
